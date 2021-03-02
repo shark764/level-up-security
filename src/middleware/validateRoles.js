7e54889f2a3346fc1c2aea7bfc0f02cd76409b52
Gerardo Zarate <gerardo.zarate@focusservices.com>
2021-03-02 08:02:42 -0600

    Adding new response method to middlewares

--- src/middleware/validateRoles.js
+++ src/middleware/validateRoles.js
@@ -1,27 +1,17 @@
+const error = require('../utils/response').error
+
 const checkIsInRole = (...roles) => (req, res, next) => {
 
     if (!req.user) {
         return res
         .status(403)
-        .json({
-            request_id: req.id,
-            success: false,
-            error: {
-                message: "Not Authorized"
-            }
-        })
+        .json(error({ requestId: req.id, code: 403, message: 'Not Authorized' }))
     }
     const hasRole = roles.find(role => req.user.data.role === role)
     if (!hasRole) {
         return res
         .status(403)
-        .json({
-            request_id: req.id,
-            success: false,
-            error: {
-                message: "Not Authorized"
-            }
-        })
+        .json(error({ requestId: req.id, code: 403, message: 'Not Authorized' }))
     }
     
     return next()
