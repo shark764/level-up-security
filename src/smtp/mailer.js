b6fe3b290dccee7322f94253b97bf1eaef4dd90f
Carlos Burgos <carlos.burgos2@focusservices.com>
2021-10-08 04:23:35 +0000

    Update src/smtp/mailer.js, .gitlab-ci.yml

--- src/smtp/mailer.js
+++ src/smtp/mailer.js
@@ -2,15 +2,24 @@ const NodeMailer = require('nodemailer');
 const { DEV_MODE } = require('../utils/consts');
 
 const transport = NodeMailer.createTransport(
-  {
-    host: process.env.SERVICE,
-    secure: true,
-    port: process.env.SERVICE_PORT,
-    auth: {
-      user: process.env.USER_SERVICE,
-      pass: process.env.USER_SERVICE_PASSWORD,
-    },
-  } 
+  /*DEV_MODE
+    ? {
+        host: process.env.SMTP_HOST,
+        port: parseInt(process.env.SMTP_PORT),
+        auth: {
+          user: process.env.SMTP_USER,
+          pass: process.env.SMTP_PASS,
+        },
+      }
+    :*/ {
+        host: process.env.SERVICE,
+        secure: true,
+        port: process.env.SERVICE_PORT,
+        auth: {
+          user: process.env.USER_SERVICE,
+          pass: process.env.USER_SERVICE_PASSWORD,
+        },
+      }
 );
 
 const sendMail = (message, callback) => {
